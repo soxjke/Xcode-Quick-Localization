1507581172a5ee532d1d99531c517935836c611f
Evgeniy Altynpara <admingeka@gmail.com>
2014-01-10 21:45:38 +0200

    added save-to-file ~/Localizible.strings

--- QuickLocalization/QuickLocalization.m
+++ QuickLocalization/QuickLocalization.m
@@ -8,6 +8,9 @@
 
 #import "QuickLocalization.h"
 #import "RCXcode.h"
+
+#define REPLACEMENT_STRINGS @[@"%@", @",", @".", @":", @"\\", @""]
+
 static NSString *localizeRegexs[] = {
     @"NSLocalizedString\\s*\\(\\s*@\"(.*)\"\\s*,\\s*(.*)\\s*\\)",
     @"localizedStringForKey:\\s*@\"(.*)\"\\s*value:\\s*(.*)\\s*table:\\s*(.*)",
@@ -16,26 +19,46 @@ static NSString *localizeRegexs[] = {
     @"NSLocalizedStringWithDefaultValue\\s*\\(\\s*@\"(.*)\"\\s*,\\s*(.*)\\s*,\\s*(.*)\\s*,\\s*(.*)\\s*,\\s*(.*)\\s*\\)"
 };
 static NSString *stringRegexs = @"@\"[^\"]*\"";
+
 @implementation QuickLocalization
+
 static id sharedPlugin = nil;
 
-
-+ (void)pluginDidLoad:(NSBundle *)plugin {
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         sharedPlugin = [[self alloc] init];
     });
 }
 
-- (id)init {
-    if (self = [super init]) {
+- (NSFileHandle*)localizibleStringsFile
+{
+    NSString *path = [NSHomeDirectory() stringByAppendingPathComponent:@"Localizable.strings"];
+    return [NSFileHandle fileHandleForWritingAtPath:path];
+}
+
+- (id)init
+{
+    if (self = [super init])
+    {
         NSMenuItem *viewMenuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
-        if (viewMenuItem) {
+        if (viewMenuItem)
+        {
             [[viewMenuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *sample = [[NSMenuItem alloc] initWithTitle:@"Quick Localization" action:@selector(quickLocalization) keyEquivalent:@"d"];
+            NSMenuItem *sample = [[NSMenuItem alloc] initWithTitle:@"Быстрая локализация" action:@selector(quickLocalization) keyEquivalent:@"d"];
             [sample setKeyEquivalentModifierMask:NSShiftKeyMask | NSAlternateKeyMask];
             [sample setTarget:self];
             [[viewMenuItem submenu] addItem:sample];
+            
+            NSString *path = [NSHomeDirectory() stringByAppendingPathComponent:@"Localizable.strings"];
+            
+            FILE *fout = fopen([path cStringUsingEncoding:NSASCIIStringEncoding], "wt");
+            
+            if (fout)
+            {
+                fclose(fout);
+            }
         }
     }
     return self;
@@ -71,11 +94,41 @@ static id sharedPlugin = nil;
             }
             NSString *string = [line substringWithRange:matchedRangeInLine];
 //            NSLog(@"string index:%d, %@", i, string);
-            NSString *outputString = [NSString stringWithFormat:@"NSLocalizedString(%@, %@)", string, string];
+            
+            __block NSString *keyName = string;
+            
+            [REPLACEMENT_STRINGS enumerateObjectsUsingBlock:^(NSString *stringForReplacement, NSUInteger idx, BOOL *stop)
+            {
+                keyName = [keyName stringByReplacingOccurrencesOfString:stringForReplacement withString:@"" options:NSCaseInsensitiveSearch range:NSMakeRange(0, keyName.length)];
+            }];
+            
+            keyName = [keyName stringByReplacingOccurrencesOfString:@" " withString:@"_" options:NSCaseInsensitiveSearch range:NSMakeRange(0, keyName.length)];
+
+            NSString *outputString = [NSString stringWithFormat:@"NSLocalizedString(%@, %@)", keyName, string];
+            
             addedLength = addedLength + outputString.length - string.length;
-            if ([textView shouldChangeTextInRange:matchedRangeInDocument replacementString:outputString]) {
+            
+            if ([textView shouldChangeTextInRange:matchedRangeInDocument replacementString:outputString])
+            {
                 [textView.textStorage replaceCharactersInRange:matchedRangeInDocument
                                           withAttributedString:[[NSAttributedString alloc] initWithString:outputString]];
+                
+                NSFileHandle *fh = [self localizibleStringsFile];
+                
+                if (!fh)
+                {
+                    NSRunAlertPanel(@"", @"", @"OK", @"", nil);
+                }
+                
+                [fh seekToEndOfFile];
+                
+                NSString *keyValueString =
+                [NSString stringWithFormat:@"%@ = %@;\n", [keyName substringFromIndex:1], [string substringFromIndex:1]];
+                
+                [fh writeData:[keyValueString dataUsingEncoding:NSUTF8StringEncoding]];
+                
+                [fh closeFile];
+                
                 [textView didChangeText];
             }
             
